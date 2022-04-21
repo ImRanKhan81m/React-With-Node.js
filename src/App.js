50c8c85c4e84db2ad7766780ac2fc5a5cc3f386d
Imran Hossen <freelancerimran81@gmail.com>
2022-04-21 17:17:56 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import { useEffect, useState } from 'react';
 
@@ -9,17 +9,32 @@ function App() {
     fetch('http://localhost:5000/user')
     .then(res=> res.json())
     .then(data => setUsers(data))
-  },[])
+  },[]) ;
+
+
+  const handleAddUser = event =>{
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    console.log(name, email);
+  }
+
+
   return (
     <div className="App">
       <h1>My Own Data: {users.length}</h1>
+      <form onSubmit={handleAddUser}>
+        <input type="text" name="name" id="" placeholder='name' />
+        <input type="email" name="email" id="" placeholder='Email' />
+        <input type="submit" value="Add User" />
+      </form>
       <ul>
         {
-          users.map(user=><li key={user.id}> Id: {user.id} Name: {user.name} Email: {user.email}</li>)
+          users.map(user=><li key={user.id}> id: {user.id} Name: {user.name} Email: {user.email}</li>)
         }
       </ul>
     </div>
   );
 }
 
-export default App;
+export default App; 
